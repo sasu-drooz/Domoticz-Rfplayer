934e70a905de60bd1dc82d27386d0db949bffb9e
Lucas Nusssbaum <lucas.nussbaum@loria.fr>
2017-09-28 22:41:30 +0200

    Add the channel to the name of the device
    This makes it easier to match data with the real probes

--- plugin.py
+++ plugin.py
@@ -838,6 +838,7 @@ def DecodeInfoType4(DecData, infoType):
 		protocol = DecData['frame']['header']['protocol']
 		id_PHY = DecData['frame']['infos']['id_PHY']
 		adr_channel = DecData['frame']['infos']['adr_channel']
+		channel = DecData['frame']['infos']['channel']
 		qualifier = DecData['frame']['infos']['qualifier']
 		try:
 			lowBatt = DecData['frame']['infos']['lowBatt']
@@ -866,7 +867,7 @@ def DecodeInfoType4(DecData, infoType):
 				nbrdevices=x
 		if IsCreated == False and Parameters["Mode4"] == "True":
 			nbrdevices=nbrdevices+1
-			Domoticz.Device(Name="Temp - " + adr_channel, Unit=nbrdevices, Type=80, Switchtype=0).Create()
+			Domoticz.Device(Name="Temp - " + adr_channel + ' (channel ' + channel + ')', Unit=nbrdevices, Type=80, Switchtype=0).Create()
 			Devices[nbrdevices].Update(nValue = 1,sValue = str(temp),Options = Options)
 		elif IsCreated == True :
 			Devices[nbrdevices].Update(nValue = 1,sValue = str(temp))
@@ -886,7 +887,7 @@ def DecodeInfoType4(DecData, infoType):
 				nbrdevices=x
 		if IsCreated == False and Parameters["Mode4"] == "True":
 			nbrdevices=nbrdevices+1
-			Domoticz.Device(Name="Hygro - " + adr_channel, Unit=nbrdevices, Type=81, Switchtype=0).Create()
+			Domoticz.Device(Name="Hygro - " + adr_channel + ' (channel ' + channel + ')', Unit=nbrdevices, Type=81, Switchtype=0).Create()
 			Devices[nbrdevices].Update(nValue = int(hygro),sValue = "1",Options = Options)
 		elif IsCreated == True :
 			Devices[nbrdevices].Update(nValue = int(hygro),sValue = "1")
@@ -906,7 +907,7 @@ def DecodeInfoType4(DecData, infoType):
 				nbrdevices=x
 		if IsCreated == False and Parameters["Mode4"] == "True":
 			nbrdevices=nbrdevices+1
-			Domoticz.Device(Name="Temp/Hygro - " + adr_channel, Unit=nbrdevices, Type=82, Switchtype=0).Create()
+			Domoticz.Device(Name="Temp/Hygro - " + adr_channel + ' (channel ' + channel + ')', Unit=nbrdevices, Type=82, Switchtype=0).Create()
 			Devices[nbrdevices].Update(nValue = 1,sValue = str(temphygro),Options = Options)
 		elif IsCreated == True :
 			Devices[nbrdevices].Update(nValue = 1,sValue = str(temphygro))
